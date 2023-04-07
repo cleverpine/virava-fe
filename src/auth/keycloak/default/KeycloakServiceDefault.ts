ddf19be862e9e9e14e9099cc015a418f949b5ba5
Yordan Atanasov <yordan.atanasov@cleverpine.com>
2023-04-07 10:17:39 +0300

    [Default flow] Added updateToken method for refreshing the token when expired

--- src/auth/keycloak/default/KeycloakServiceDefault.ts
+++ src/auth/keycloak/default/KeycloakServiceDefault.ts
@@ -3,11 +3,13 @@ import { KeycloakConfigDefault } from './KeycloakConfigDefault';
 import { AuthResponse } from '../../../models/AuthResponse';
 import { TokenObjectValues } from '../../../models/TokenObjectValues';
 import { setTokens } from '../../../utils/helpers';
+import { MS_CHECK_TOKEN_VALIDITY_INTERVAL, TOKEN_MIN_VALIDITY } from '../../../utils/constants';
 
 import { AuthServiceBase } from '../../../auth/base/AuthServiceBase';
 
 export class KeycloakServiceDefault extends AuthServiceBase<KeycloakConfigDefault> {
   private keycloak!: Keycloak;
+  private updateTokenInterval!: ReturnType<typeof setInterval>;
 
   /**
    * Initialises the auth service configuration
@@ -42,17 +44,44 @@ export class KeycloakServiceDefault extends AuthServiceBase<KeycloakConfigDefaul
           access: this.keycloak.token,
           refresh: this.keycloak.refreshToken,
         } as TokenObjectValues);
+
+        // Check token validity every MS_CHECK_TOKEN_VALIDITY_INTERVAL seconds
+        // and if necessary, update the token.
+        this.updateTokenInterval = setInterval(this.updateToken, MS_CHECK_TOKEN_VALIDITY_INTERVAL);
       });
   };
 
   /**
-   * Logouts user and remove tokens from `localStorage`
+   * Refresh token if it's valid for less then TOKEN_MIN_VALIDITY seconds.
+   * If it is refreshed - updates the values in `localStorage`.
+   */
+  private updateToken = () => {
+    this.keycloak
+      .updateToken(TOKEN_MIN_VALIDITY)
+      .then((refreshed: boolean) => {
+        if (refreshed) {
+          setTokens(this.config, {
+            access: this.keycloak.token,
+            refresh: this.keycloak.refreshToken,
+          } as TokenObjectValues);
+        }
+      })
+      .catch((error: any) => {
+        this.logout();
+        throw new Error('Failed to refresh token ' + error);
+      });
+  }
+
+  /**
+   * Logouts user, clears the `updateTokenInterval` and removes tokens from `localStorage`
    * @param redirectUri - url to be redirected after logout
    */
   logout = async (redirectUri?: string): Promise<void> => {
     if (!this.config?.clientId || !this.config.baseUrl || !this.keycloak) {
       throw new Error('Service not initialized!');
     }
+
+    clearInterval(this.updateTokenInterval);
 
     return this.keycloak.logout({ redirectUri: redirectUri });
   };
