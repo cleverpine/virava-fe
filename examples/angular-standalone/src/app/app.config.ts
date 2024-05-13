7476304b7652b31db01844f336915539a98aee7c
Simona Georgieva <simona.georgieva@cleverpine.com>
2024-05-13 12:48:31 +0300

    add translation set up

--- examples/angular-standalone/src/app/app.config.ts
+++ examples/angular-standalone/src/app/app.config.ts
@@ -1,20 +1,35 @@
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { APP_INITIALIZER, ApplicationConfig } from '@angular/core';
+import { HttpClient, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { APP_INITIALIZER, ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import {
+  TranslateLoader,
+  TranslateModule,
+  TranslateService,
+} from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 // import { ApiConfiguration } from '@openapi/api-configuration';
 
+import { firstValueFrom } from "rxjs";
 import { routes } from './app.routes';
 import { authInterceptor } from "./core/interceptors/auth.interceptor";
 import { AppConfigService } from "./core/services/app-config.service";
 import { AuthService } from "./core/services/auth.service";
 
+function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http);
+}
+
 const initializeApp = (
   appConfigService: AppConfigService,
   // apiConfiguration: ApiConfiguration,
   authService: AuthService,
+  translate: TranslateService
 ) => {
   return async () => {
+    translate.setDefaultLang('en');
+    await firstValueFrom(translate.use('en'));
+
     await appConfigService.loadConfig();
     // apiConfiguration.rootUrl = appConfigService.getBasePath();
     const keycloakUrl = appConfigService.getSsoServerUrl();
@@ -32,9 +47,18 @@ export const appConfig: ApplicationConfig = {
     {
       provide: APP_INITIALIZER,
       useFactory: initializeApp,
-      deps: [AppConfigService, AuthService],
+      deps: [AppConfigService, AuthService, TranslateService],
       multi: true,
     },
     provideHttpClient(withInterceptors([authInterceptor])),
+    importProvidersFrom(
+      TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+      }),
+    )
   ],
 };
